b311950aaf695e16313dd9abb18dfb197db355cf
Luco97 <renatoluco@gmail.com>
2023-02-14 21:03:56 -0300

    ADD propiedades para el modelo de categorias

--- src/database/tag/tag-model.ts
+++ src/database/tag/tag-model.ts
@@ -1,1 +1,23 @@
-export class TagModel {}
+import {
+  Entity,
+  Property,
+  PrimaryKey,
+  ManyToMany,
+  Collection,
+} from '@mikro-orm/core';
+import { ItemModel } from '../item/item-model';
+
+@Entity()
+export class TagModel {
+  @PrimaryKey()
+  id: number;
+
+  @Property()
+  name!: string;
+
+  @Property()
+  description: string;
+
+  @ManyToMany(() => ItemModel, (item) => item.tags)
+  items: Collection<ItemModel> = new Collection<ItemModel>(this);
+}
