0d236bf3c8fa44fe675f92e615a53613beb4de50
Luco97 <renatoluco@gmail.com>
2023-02-14 21:04:08 -0300

    ADD propiedades para el modelo de usuario

--- src/database/user/user-model.ts
+++ src/database/user/user-model.ts
@@ -1,1 +1,31 @@
-export class UserModel {}
+import {
+  Collection,
+  Entity,
+  OneToMany,
+  PrimaryKey,
+  Property,
+  Enum,
+} from '@mikro-orm/core';
+import { ItemModel } from '../item/item-model';
+import { UserTypes } from './user.enum';
+
+@Entity()
+export class UserModel {
+  @PrimaryKey()
+  id: number;
+
+  @Property()
+  email!: string;
+
+  @Property()
+  username!: string;
+
+  @Property({ lazy: true })
+  password!: string;
+
+  @Enum(() => UserTypes)
+  type!: string;
+
+  @OneToMany(() => ItemModel, (item) => item.user)
+  items: Collection<ItemModel> = new Collection<ItemModel>(this);
+}
