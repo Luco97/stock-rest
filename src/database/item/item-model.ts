48af8b773821bfe7ce12147f843e4d316e9c72e2
Luco97 <renatoluco@gmail.com>
2023-02-14 21:03:44 -0300

    ADD propiedades para el modelo de productos

--- src/database/item/item-model.ts
+++ src/database/item/item-model.ts
@@ -1,1 +1,44 @@
-export class ItemModel {}
+import { Entity } from '@mikro-orm/core/decorators';
+import {
+  ManyToMany,
+  Collection,
+  ManyToOne,
+  OneToMany,
+  Property,
+} from '@mikro-orm/core';
+
+import { TagModel } from '../tag/tag-model';
+import { UserModel } from '../user/user-model';
+import { HistoricModel } from '../historic/historic-model';
+
+@Entity()
+export class ItemModel {
+  id: number;
+
+  @Property()
+  name!: string;
+
+  @Property()
+  stock: number;
+
+  @Property()
+  price: number;
+
+  @Property()
+  imageUrl: string;
+
+  @Property()
+  createdAt: Date = new Date();
+
+  @Property({ onUpdate: () => new Date() })
+  updatedAt: Date = new Date();
+
+  @ManyToOne(() => UserModel)
+  user!: UserModel;
+
+  @OneToMany(() => HistoricModel, (historic) => historic.item)
+  changes: Collection<HistoricModel> = new Collection<HistoricModel>(this);
+
+  @ManyToMany(() => TagModel, 'items', { owner: true })
+  tags: Collection<TagModel> = new Collection<TagModel>(this);
+}
