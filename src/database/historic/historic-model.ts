d111669a3c78d77530cebe11878f8e019e8f89c2
Luco97 <renatoluco@gmail.com>
2023-02-14 21:03:27 -0300

    ADD propiedades para el modelo de historico

--- src/database/historic/historic-model.ts
+++ src/database/historic/historic-model.ts
@@ -1,1 +1,20 @@
-export class HistoricModel {}
+import { ItemModel } from '../item/item-model';
+import { ManyToOne, Property, PrimaryKey, Entity } from '@mikro-orm/core';
+
+@Entity()
+export class HistoricModel {
+  @PrimaryKey()
+  id: number;
+
+  @Property()
+  change!: string;
+
+  @Property({ nullable: true })
+  previousValue: string;
+
+  @Property({ onCreate: () => new Date() })
+  createdAt: Date;
+
+  @ManyToOne(() => ItemModel)
+  item!: ItemModel;
+}
