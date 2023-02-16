7360cd9ff1d49cf164f28c4d185e479012ee1f24
Luco97 <renatoluco@gmail.com>
2023-02-16 19:31:49 -0300

    ADD archivo de configuracion para mikro-orm

---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,19 @@
+import { LoadStrategy } from '@mikro-orm/core';
+import { defineConfig } from '@mikro-orm/postgresql';
+import { TsMorphMetadataProvider } from '@mikro-orm/reflection';
+
+// Texto bonito en consola
+import { SqlHighlighter } from '@mikro-orm/sql-highlighter';
+import { Migrator } from '@mikro-orm/migrations';
+import { EntityGenerator } from '@mikro-orm/entity-generator';
+import { SeedManager } from '@mikro-orm/seeder';
+
+export default defineConfig({
+  debug: true,
+  loadStrategy: LoadStrategy.JOINED,
+  highlighter: new SqlHighlighter(),
+  metadataProvider: TsMorphMetadataProvider,
+  // @ts-expect-error nestjs adapter option
+  registerRequestContext: false,
+  extensions: [Migrator, EntityGenerator, SeedManager],
+});
