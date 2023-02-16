b788ef13731881e53b85a05fddc32c2fb4f2d04e
Luco97 <renatoluco@gmail.com>
2023-02-16 19:33:28 -0300

    ADD se mueven modelos un nivel y se utilizan en app

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+
+import { UserModule } from './user/user.module';
+import { ItemModule } from './item/item.module';
+import { TagModule } from './tag/tag.module';
+import { HistoricModule } from './historic/historic.module';
+
 @Module({
-  imports: [],
+  imports: [
+    MikroOrmModule.forRoot(),
+    UserModule,
+    ItemModule,
+    TagModule,
+    HistoricModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
