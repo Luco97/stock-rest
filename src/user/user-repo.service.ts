b2b91641e4448906f622a8e046c17441c62df755
Luco97 <renatoluco@gmail.com>
2023-02-19 04:05:48 -0300

    ADD metodo para creacion de usuario

--- src/user/user-repo.service.ts
+++ src/user/user-repo.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectRepository } from '@mikro-orm/nestjs';
+import { EntityRepository } from '@mikro-orm/postgresql';
+
+import { UserModel } from './user-model';
+import { UserTypes } from './user.enum';
 
 @Injectable()
-export class UserRepoService {}
+export class UserRepoService {
+  private readonly _logger = new Logger(UserRepoService.name);
+  constructor(
+    @InjectRepository(UserModel)
+    private readonly _userRepo: EntityRepository<UserModel>,
+  ) {}
+
+  async create(params: { email: string; username: string; password: string }) {
+    const { email, password, username } = params;
+    return this._userRepo.persistAndFlush(
+      this._userRepo.create({
+        email,
+        username,
+        password,
+        type: UserTypes.BASIC,
+      }),
+    );
+  }
+}
