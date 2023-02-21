bb60090c1675c84f1a2bce2474ef320ac4933340
Luco97 <renatoluco@gmail.com>
2023-02-21 20:03:25 -0300

    ADD entidad virtual para contar numero de items por tag

--- src/tag/tag-model.ts
+++ src/tag/tag-model.ts
@@ -5,6 +5,7 @@ import {
   ManyToMany,
   Collection,
 } from '@mikro-orm/core';
+import { EntityManager } from '@mikro-orm/postgresql';
 import { ItemModel } from '../item/item-model';
 
 @Entity()
@@ -20,4 +21,32 @@ export class TagModel {
 
   @ManyToMany(() => ItemModel, (item) => item.tags)
   items: Collection<ItemModel> = new Collection<ItemModel>(this);
+}
+
+@Entity({
+  expression: (em: EntityManager) => {
+    return em
+      .createQueryBuilder(TagModel, 'tag')
+      .select([
+        'tag.id as id',
+        'tag.name as name',
+        'tag.description as description',
+        'COUNT(items.id) as item_count',
+      ])
+      .leftJoin('tag.items', 'items')
+      .groupBy('tag.id');
+  },
+})
+export class TagItemsCount {
+  @Property()
+  id: number;
+
+  @Property()
+  name: string;
+
+  @Property()
+  itemCount: number;
+
+  @Property()
+  description: string;
 }
