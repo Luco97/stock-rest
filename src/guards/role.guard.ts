084bd97b1875e292eef7c83b3f8d1850ffcdbcce
Luco97 <renatoluco@gmail.com>
2023-02-25 15:31:14 -0300

    ADD logica para permitir usuarios segun tipo

--- src/guards/role.guard.ts
+++ src/guards/role.guard.ts
@@ -1,12 +1,33 @@
+import { Reflector } from '@nestjs/core';
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { Observable } from 'rxjs';
+
+import { FastifyRequest } from 'fastify';
+import { from, map, Observable, of } from 'rxjs';
+
+import { AuthService } from '@shared/auth';
+import { UserRepoService } from '../user/user-repo.service';
 
 @Injectable()
 export class RoleGuard implements CanActivate {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    const request: Request = context.switchToHttp().getRequest();
-    return true;
+  constructor(
+    private readonly _reflector: Reflector,
+    private readonly _authService: AuthService,
+    private readonly _userService: UserRepoService,
+  ) {}
+  canActivate(context: ExecutionContext): boolean | Observable<boolean> {
+    const request: FastifyRequest = context.switchToHttp().getRequest();
+    const roles: string[] = this._reflector.get<string[]>(
+      'roles',
+      context.getHandler(),
+    );
+
+    if (!this._authService.validateToken(request.headers?.authorization))
+      of(false);
+
+    const userID = this._authService.userID(request.headers?.authorization);
+
+    return from(this._userService.countRoles({ userID, roles })).pipe(
+      map<number, boolean>((count) => (count ? true : false)),
+    );
   }
 }
