84fd3ed4cb218186522d13ad6b39c57e9d81dc6f
Luco97 <renatoluco@gmail.com>
2023-02-26 14:19:45 -0300

    CREATE index para import bonito de DTO

---
+++ src/classes/item/index.ts
@@ -0,0 +1,3 @@
+export * from './update.dto';
+export * from './get-items.dto';
+export * from './create-item.dto';
---
+++ src/classes/tag/index.ts
@@ -0,0 +1,3 @@
+export * from './read-tag.dto';
+export * from './create-tag.dto';
+export * from './update-tag.dto';
