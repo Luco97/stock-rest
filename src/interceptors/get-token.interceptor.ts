edb99a9dc969418b9234ce82151f5322e19359b5
Luco97 <renatoluco@gmail.com>
2023-02-27 19:20:16 -0300

    ADD en caso de nececsitar info preparada de usuario

--- src/interceptors/get-token.interceptor.ts
+++ src/interceptors/get-token.interceptor.ts
@@ -1,9 +1,28 @@
-import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import {
+  Injectable,
+  CallHandler,
+  NestInterceptor,
+  ExecutionContext,
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { FastifyRequest } from 'fastify';
+
+import { AuthService } from '@shared/auth';
 
 @Injectable()
 export class GetTokenInterceptor implements NestInterceptor {
+  constructor(private _authService: AuthService) {}
+
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const request: FastifyRequest = context.switchToHttp().getRequest();
+    const { id, name, type } = this._authService.getContext(
+      request.headers?.authorization,
+    );
+
+    request.headers['user_id'] = id.toString();
+    request.headers['user_name'] = name;
+    request.headers['user_type'] = type;
+
     return next.handle();
   }
 }
