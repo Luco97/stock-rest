48ec71ecb2507fcee7f0e38ca62bbe1df9b48a3c
Luco97 <renatoluco@gmail.com>
2023-03-04 01:55:00 -0300

    UPDATE se quitan dtos sin uso para items

--- src/classes/item/update.dto.ts
+++ src/classes/item/update.dto.ts
@@ -7,15 +7,7 @@ import {
   MinLength,
 } from 'class-validator';
 
-export class UpdateInput {
-  @Min(1)
-  id_item: number;
-}
-
-export class Update {
-  @Min(1)
-  id_item: number;
-
+export class UpdateItem {
   @IsOptional()
   @Min(0)
   price: number;
@@ -31,13 +23,4 @@ export class Update {
   @IsOptional()
   @MinLength(5)
   name: string;
-}
-
-export class UpdateTags {
-  @Min(1)
-  id_item: number;
-
-  @IsArray()
-  @ArrayMinSize(1)
-  tags_id: number[];
 }
