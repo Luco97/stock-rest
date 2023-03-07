389b80599acbb7ee6bb84340a5e736b7eb742e6b
Luco97 <renatoluco@gmail.com>
2023-03-07 19:36:35 -0300

    FIX se agrega elvis para evitar posible error en caso de ser undefined

--- src/shared/pipes/alphanumeric.pipe.ts
+++ src/shared/pipes/alphanumeric.pipe.ts
@@ -8,7 +8,7 @@ import {
 @Injectable()
 export class AlphanumericPipe implements PipeTransform {
   transform(value: string, metadata: ArgumentMetadata) {
-    if (value.match(/[^ \w]/)?.length)
+    if (value?.match(/[^ \w]/)?.length)
       throw new BadRequestException(`no symbols allowed in ${metadata.data}`);
     return value;
   }
