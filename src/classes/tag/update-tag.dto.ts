14dc91439e5525c0cede0b374d7b7aa79f0d8a39
Luco97 <renatoluco@gmail.com>
2023-03-07 19:34:02 -0300

    UPDATE se quita id pra class-validator de update-tag

--- src/classes/tag/update-tag.dto.ts
+++ src/classes/tag/update-tag.dto.ts
@@ -1,9 +1,6 @@
-import { IsOptional, Min, MinLength } from 'class-validator';
+import { IsOptional, MinLength } from 'class-validator';
 
 export class UpdateTags {
-  @Min(1)
-  id_tag: number;
-
   @IsOptional()
   @MinLength(5)
   description: string;
