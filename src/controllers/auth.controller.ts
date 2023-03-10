c9afd66cb159faedec8dbdc46eaa0323b5602f95
Luco97 <renatoluco@gmail.com>
2023-03-10 19:03:34 -0300

    UPDATE se usan metodos de servicio para usuario

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,67 +2,30 @@ import { Controller, Post, Body, Res, HttpStatus } from '@nestjs/common';
 import { FastifyReply } from 'fastify';
 
 import { Register, SignIn } from '@dto/auth';
-import { UserRepoService } from '@models/user';
-import { compare } from 'bcrypt';
-import { AuthService } from '@shared/auth';
+import { UserService } from '../services/user.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(
-    private _userRepo: UserRepoService,
-    private _authService: AuthService,
-  ) {}
+  constructor(private _userService: UserService) {}
 
   @Post('register') registerUser(
     @Body() register_user: Register,
     @Res() res: FastifyReply,
   ) {
     const { email, password, username } = register_user;
-
-    this._userRepo
-      .findUser({ email, username: username.toLowerCase() })
-      .then((user) => {
-        if (user)
-          res
-            .status(HttpStatus.OK)
-            .send({ status: HttpStatus.OK, message: 'already exist' });
-        else
-          this._userRepo
-            .create({ email, password, username })
-            .then(() =>
-              res
-                .status(HttpStatus.OK)
-                .send({ status: HttpStatus.OK, message: 'user created' }),
-            );
-      });
+    this._userService.register({ email, password, username }).then((isValid) =>
+      res.status(HttpStatus.OK).send({
+        status: HttpStatus.OK,
+        message: isValid ? 'user created' : 'already exist',
+      }),
+    );
   }
 
   @Post('sign') signIn(@Body() sign_user: SignIn, @Res() res: FastifyReply) {
     const { email, password } = sign_user;
 
-    this._userRepo.findUser({ email, username: ' ' }).then((user) => {
-      if (!user)
-        res
-          .status(HttpStatus.OK)
-          .send({ status: HttpStatus.OK, message: `user doesn't exist` });
-      else {
-        compare(password, user.password).then((isValid) => {
-          if (!isValid)
-            res
-              .status(HttpStatus.OK)
-              .send({ status: HttpStatus.OK, message: `user doesn't exist` });
-          else
-            res.status(HttpStatus.OK).send({
-              status: HttpStatus.OK,
-              message: 'user logged',
-              token: this._authService.genJWT({
-                id: user.id,
-                name: user.username,
-                type: user.type,
-              }),
-            });
-        });
-      }
-    });
+    this._userService
+      .signIn({ email, password })
+      .then((response) => res.status(response.status).send(response));
   }
 }
