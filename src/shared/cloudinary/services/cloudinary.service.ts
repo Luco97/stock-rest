08cfcabb961e9d26fb363a0c94e90a6a9ef5c047
Luco97 <renatoluco@gmail.com>
2023-03-28 21:12:09 -0300

    ADD se define cloudinary con variables de configuracion y se crea metodo para subir imagen

--- src/shared/cloudinary/services/cloudinary.service.ts
+++ src/shared/cloudinary/services/cloudinary.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { v2, UploadApiResponse } from 'cloudinary';
+
+import { CloudinaryConfig } from '../interface/cloudinary-config.interface';
 
 @Injectable()
-export class CloudinaryService {}
+export class CloudinaryService {
+  private _cloudinary = v2;
+  constructor(
+    @Inject('CLOUDINARY_CONFIG') private _cloudinaryConfig: CloudinaryConfig,
+  ) {
+    this._cloudinary.config({
+      cloud_name: _cloudinaryConfig.cloud_name,
+      api_key: _cloudinaryConfig.api_key,
+      api_secret: _cloudinaryConfig.api_secret,
+      secure: _cloudinaryConfig.secure,
+    });
+  }
+
+  upload(
+    filePath: string,
+    folderParth: string,
+    fileName: string,
+  ): Promise<UploadApiResponse> {
+    return this._cloudinary.uploader.upload(filePath, {
+      folder: folderParth,
+      public_id: fileName,
+    });
+  }
+}
