a3989dcebbf4973a925ac14fa8aa2562cef08661
Luco97 <renatoluco@gmail.com>
2023-03-28 21:10:34 -0300

    ADD configuracion para uso de modulo como si fuese una libreria ajena

--- src/shared/cloudinary/cloudinary.module.ts
+++ src/shared/cloudinary/cloudinary.module.ts
@@ -1,8 +1,24 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
 import { CloudinaryService } from './services/cloudinary.service';
 
 @Module({
   exports: [CloudinaryService],
   providers: [CloudinaryService],
 })
-export class CloudinaryModule {}
+export class CloudinaryModule {
+  forRoot(config: {
+    cloud_name: string;
+    api_key: string;
+    api_secret: string;
+    secure: boolean;
+  }): DynamicModule {
+    return {
+      module: CloudinaryModule,
+      providers: [
+        { provide: 'CLOUDINARY_CONFIG', useValue: config },
+        CloudinaryService,
+      ],
+      exports: [CloudinaryService],
+    };
+  }
+}
