e46509dd3209267789ddcd1e298b61ac499fd5ef
Luco97 <renatoluco@gmail.com>
2023-04-02 18:16:59 -0400

    UPDATE se quita url de imagen (imagenes se suben a cloudinary)

--- src/classes/item/create-item.dto.ts
+++ src/classes/item/create-item.dto.ts
@@ -1,17 +1,14 @@
-import { IsDefined, IsOptional, Min, MinLength } from 'class-validator';
+import { IsDefined, Min, MinLength } from 'class-validator';
 
 export class CreateItem {
   @IsDefined()
   @MinLength(5)
   name: string;
-  
-  @IsOptional()
-  imageUrl: string;
-  
+
   @IsDefined()
   @Min(0)
   stock: number;
-  
+
   @IsDefined()
   @Min(0)
   price: number;
