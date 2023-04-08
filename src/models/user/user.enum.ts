e6ca9b5f0324c63926d23d194ca520cc1662f815
Luco97 <renatoluco@gmail.com>
2023-04-08 11:59:56 -0400

    ADD se crean constantes y enum para tipos de usuarios aplicables

--- src/models/user/user.enum.ts
+++ src/models/user/user.enum.ts
@@ -1,6 +1,14 @@
 export enum UserTypes {
-  ADMIN = 'admin',
   BASIC = 'basic',
+  ADMIN = 'admin',
   MOD = 'mod',
   MASTER = 'master',
 }
+
+export enum UserTypesOptionsEnum {
+  BASIC = 'basic',
+  ADMIN = 'admin',
+  MOD = 'mod',
+}
+
+export const UserTypesOptions: string[] = ['basic', 'admin', 'mod'];
