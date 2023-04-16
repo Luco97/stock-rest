692d18594716f317d324f05ead622da8e4f345ad
Luco97 <renatoluco@gmail.com>
2023-04-16 16:50:24 -0400

    UPDATE se meuve funcion para hash de password

--- src/models/user/user.enum.ts
+++ src/models/user/user.enum.ts
@@ -1,3 +1,5 @@
+import { genSalt, hash } from 'bcrypt';
+
 export enum UserTypes {
   BASIC = 'basic',
   ADMIN = 'admin',
@@ -12,3 +14,15 @@ export enum UserTypesOptionsEnum {
 }
 
 export const UserTypesOptions: string[] = ['basic', 'admin', 'mod'];
+
+export async function hashPass(pass: string): Promise<string> {
+  const saltRound: number = 10;
+  const newPassword = pass || this.password;
+  return new Promise<string>((resolve, reject) => {
+    genSalt(saltRound).then((bcSaltRound) =>
+      hash(newPassword, bcSaltRound)
+        .then((hashPass) => resolve(hashPass))
+        .catch((error) => reject(error)),
+    );
+  });
+}
