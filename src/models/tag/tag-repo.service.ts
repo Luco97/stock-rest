fd455985bde3039a3cf75319e3a84535f7a63f19
Luco97 <renatoluco@gmail.com>
2023-04-25 19:30:09 -0400

    FIX se quita incluir vacio en regex y se cambia por plus(+)

--- src/models/tag/tag-repo.service.ts
+++ src/models/tag/tag-repo.service.ts
@@ -47,7 +47,7 @@ export class TagRepoService {
 
     return new Promise<TagModel>((resolve, reject) => {
       const newTag: TagModel = this._tagRepo.create({
-        name: name.replace(/[^A-Za-z0-9 | ñ]*/g, '-'),
+        name: name.replace(/[^A-Za-z0-9|ñ]+/g, '-'),
         description,
       });
       this._tagRepo.persistAndFlush(newTag).then(() => resolve(newTag));
